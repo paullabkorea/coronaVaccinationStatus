ef4c606f290f78fc260581fdc3b939f05cda9041
hojun.lee <60565211+paullabkorea@users.noreply.github.com>
2021-06-29 06:52:17 +0900

    Update mainDataCrawler.py

--- data/mainDataCrawler.py
+++ data/mainDataCrawler.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 table = pd.read_html('https://ncv.kdca.go.kr/mainStatus.es?mid=a11702000000', encoding='utf-8')
-인구 = pd.read_csv('시도별_주민등록_인구현황.csv', encoding='cp949')
+인구 = pd.read_csv('./populationData.csv', encoding='cp949')
 
 인구['1차 접종 누계'] = table[1]['1회차 접종']['당일 누계']
 인구['2차 접종 누계'] = table[1]['2회차 접종']['당일 누계']
